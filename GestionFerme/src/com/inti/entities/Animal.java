7a5a2cd161b743682af97b089cc44abe9598b62d
hugohavez <arby.havez@gmail.com>
2020-10-08 15:16:02 +0200

    correction package entities

--- GestionFerme/src/com/inti/entities/Animal.java
+++ GestionFerme/src/com/inti/entities/Animal.java
@@ -2,29 +2,37 @@ package com.inti.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+import javax.persistence.OneToOne;
 
 
 @Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="role",discriminatorType=DiscriminatorType.STRING)
 public class Animal  implements Serializable{
 
-
-	long idAnimal;
-	boolean etat;
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private  static  final  long serialVersionUID = 1L;
 	
+	long idAnimal;
+	boolean etat;
 	
-	@ManyToOne
-	private  Animal Ferme;
+	@OneToOne
+	private Ferme ferme;
 
-
+	@OneToOne
+	private Eleveur eleveur;
+	
+	
 	public Animal() {
 		super();
 	}
@@ -50,14 +58,23 @@ public class Animal  implements Serializable{
 	}
 
 
-	public Animal getFerme() {
-		return Ferme;
+	public Ferme getFerme() {
+		return ferme;
 	}
 
 
-	public void setFerme(Animal ferme) {
-		Ferme = ferme;
+	public void setFerme(Ferme ferme) {
+		this.ferme = ferme;
 	}
-	
+
+
+	public Eleveur getEleveur() {
+		return eleveur;
+	}
+
+
+	public void setEleveur(Eleveur eleveur) {
+		this.eleveur = eleveur;
+	}
 	
 }
--- GestionFerme/src/com/inti/entities/BasseCour.java
+++ GestionFerme/src/com/inti/entities/BasseCour.java
@@ -1,12 +1,14 @@
 package com.inti.entities;
 import java.io.Serializable;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@DiscriminatorValue("basse_cour")
 public class BasseCour extends Animal implements Serializable{
 
 	String habitat;
